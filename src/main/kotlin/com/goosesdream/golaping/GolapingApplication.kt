6fcf345c9b78cbe0ea674c1c93433971bc49e33f
joonghyun <joongh@ewhain.net>
2024-12-26 20:04:19 +0900

    프로젝트 초기화

---
+++ src/main/kotlin/com/goosesdream/golaping/GolapingApplication.kt
@@ -0,0 +1,11 @@
+package com.goosesdream.golaping
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GolapingApplication
+
+fun main(args: Array<String>) {
+    runApplication<GolapingApplication>(*args)
+}
---
+++ src/test/kotlin/com/goosesdream/golaping/GolapingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.goosesdream.golaping
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GolapingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
