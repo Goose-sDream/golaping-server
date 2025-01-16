63864c253e7d9374cd9187dcf6f21531b5a7b5a4
joonghyun <joongh@ewhain.net>
2025-01-16 23:11:38 +0900

    #8 feature: 세션 사용을 위한 RedisService와 SessionService를 구현한다.

---
+++ src/main/kotlin/com/goosesdream/golaping/session/service/SessionService.kt
@@ -0,0 +1,27 @@
+package com.goosesdream.golaping.session.service
+
+import com.goosesdream.golaping.redis.service.RedisService
+import org.springframework.stereotype.Service
+
+@Service
+class SessionService(
+    private val redisService: RedisService,
+) {
+    private val nicknamePrefix = "session:nickname:"
+
+    fun saveNicknameToSession(sessionId: String, nickname: String, timeLimit: Int) {
+        val redisKey = nicknamePrefix + sessionId
+        val ttlInSeconds = timeLimit * 60L
+        redisService.save(redisKey, nickname, ttlInSeconds)
+    }
+
+    fun getNicknameFromSession(sessionId: String): String? {
+        val redisKey = nicknamePrefix + sessionId
+        return redisService.get(redisKey)
+    }
+
+    fun deleteSession(sessionId: String) {
+        val redisKey = nicknamePrefix + sessionId
+        redisService.delete(redisKey)
+    }
+}
