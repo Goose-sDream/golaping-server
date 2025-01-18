55323fcc04508e3cb8077622d89c77dc349ed861
joonghyun <joongh@ewhain.net>
2025-01-18 17:08:45 +0900

    #33 feature: 보안을 위해 redis password를 추가한다.

--- src/main/kotlin/com/goosesdream/golaping/configuration/RedisConfig.kt
+++ src/main/kotlin/com/goosesdream/golaping/configuration/RedisConfig.kt
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
@@ -16,12 +18,17 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisHttpSession
 class RedisConfig(
     @Value("\${spring.data.redis.host}") private val host: String,
-    @Value("\${spring.data.redis.port}") private val port: Int
+    @Value("\${spring.data.redis.port}") private val port: Int,
+    @Value("\${spring.data.redis.password}") private var password: String
 ) {
 
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory =
-        LettuceConnectionFactory(host, port)
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration(host, port)
+        redisStandaloneConfiguration.password = RedisPassword.of(password)
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
+    }
+
 
     @Bean
     fun redisTemplate(): RedisTemplate<String, Any> {
