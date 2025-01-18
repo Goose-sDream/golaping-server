c442b9cfd614dc977cdaeb8d10d4c027e7d30621
joonghyun <joongh@ewhain.net>
2025-01-18 22:18:43 +0900

    #27 feature: test를 위한 MockK dependency를 추가한다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j:9.1.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.mockk:mockk:1.13.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
