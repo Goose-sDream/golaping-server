cdc7f2ec64b68c3bf0b3a6970852115aae35c2e4
joonghyun <joongh@ewhain.net>
2025-01-19 22:55:48 +0900

    #49 feature: 유저별 웹소켓 채널 생성 구현 및 redis에서 웹소켓 세션을 관리할 수 있도록 수정

--- src/main/kotlin/com/goosesdream/golaping/common/websocket/GlobalWebSocketHandler.kt
+++ src/main/kotlin/com/goosesdream/golaping/common/websocket/GlobalWebSocketHandler.kt
@@ -7,19 +7,52 @@ import org.springframework.web.socket.WebSocketMessage
 import org.springframework.web.socket.WebSocketSession
 
 @Component
-class GlobalWebSocketHandler : WebSocketHandler {
-
-    private val sessions = mutableMapOf<String, WebSocketSession>()
+class GlobalWebSocketHandler(
+    private val webSocketManager: WebSocketManager
+) : WebSocketHandler{
 
     override fun afterConnectionEstablished(session: WebSocketSession) {
-
         val voteUuid = session.attributes["voteUuid"] as? String
-        val nickname = session.attributes["nickname"] as? String // 투표 종료 상태면 empty string
-        val isVoteEnded = session.attributes["isVoteEnded"] as? Boolean
 
         if (voteUuid != null) {
-            sessions[voteUuid] = session
-            println("New session for vote: $voteUuid, Nickname: $nickname, VoteEnded: $isVoteEnded")
+            var expirationTime = webSocketManager.getChannelExpirationTime(voteUuid)
+
+            if (expirationTime != null) {
+                val remainingTimeMillis = expirationTime - System.currentTimeMillis()
+
+                // 세션이 없으면(처음 접속하는 유저) 새로운 타이머 설정
+                val existingSession = webSocketManager.restoreWebSocketSession(voteUuid)
+                if (existingSession == null) {
+                    if (remainingTimeMillis > 0) {
+                        webSocketManager.setWebSocketTimer(voteUuid, remainingTimeMillis)
+                    } else {
+                        webSocketManager.stopWebSocketForVote(voteUuid)
+                    }
+                }
+            } else { // 이미 종료된 투표 또는 유효하지 않은 상태인 경우
+                webSocketManager.stopWebSocketForVote(voteUuid)
+            }
+
+            // 세션이 없으면(처음 접속하는 유저) 새로운 채널 시작
+            val existingSession = webSocketManager.restoreWebSocketSession(voteUuid)
+            if (existingSession == null) {
+                expirationTime = webSocketManager.getChannelExpirationTime(voteUuid)
+                val timeLimit = expirationTime?.let { // 투표 남은 시간만큼 웹소켓 세션 timeLimit 설정
+                    val remainingTimeMillis = it - System.currentTimeMillis()
+                    if (remainingTimeMillis > 0) {
+                        (remainingTimeMillis / 1000 / 60).toInt()
+                    } else {
+                        return@let null
+                    }
+                }
+
+                if (timeLimit == null) {
+                    webSocketManager.stopWebSocketForVote(voteUuid)
+                } else { // 새로운 채널 시작
+                    webSocketManager.startWebSocketForVote(voteUuid, timeLimit)
+                }
+            }
+            webSocketManager.saveWebSocketSession(voteUuid, session)
         }
     }
 
@@ -30,18 +63,14 @@ class GlobalWebSocketHandler : WebSocketHandler {
     override fun handleTransportError(session: WebSocketSession, exception: Throwable) {
         val voteUuid = session.attributes["voteUuid"] as? String
         if (voteUuid != null) {
-            sessions.remove(voteUuid)
-            println("Transport error for vote: $voteUuid")
+            webSocketManager.stopWebSocketForVote(voteUuid)
         }
-        println("Error occurred: ${exception.message}")
-        exception.printStackTrace()
     }
 
     override fun afterConnectionClosed(session: WebSocketSession, closeStatus: CloseStatus) {
         val voteUuid = session.attributes["voteUuid"] as? String
         if (voteUuid != null) {
-            sessions.remove(voteUuid)
-            println("Session closed for vote: $voteUuid")
+            webSocketManager.stopWebSocketForVote(voteUuid)
         }
     }
 
