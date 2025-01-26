20a86982c168b87fa8930d3ccc04f61d99dd4bbd
joonghyun <joongh@ewhain.net>
2025-01-26 23:38:09 +0900

    #73 fix: 웹소켓 최초 연결 응답에 웹소켓 세션 Id를 함께 반환하도록 수정한다.

--- src/main/kotlin/com/goosesdream/golaping/common/websocket/dto/WebSocketInitialResponse.kt
+++ src/main/kotlin/com/goosesdream/golaping/common/websocket/dto/WebSocketInitialResponse.kt
@@ -2,5 +2,6 @@ package com.goosesdream.golaping.common.websocket.dto
 
 data class WebSocketInitialResponse(
     val voteLimit: Int,
-    val voteEndTime: Long
+    val voteEndTime: Long,
+    val webSocketSessionId: String? = null
 )
