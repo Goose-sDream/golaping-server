ca979266acb21630c1a2456b4ac51a9f5e89ae64
joonghyun <joongh@ewhain.net>
2025-01-27 11:14:23 +0900

    #77 fix: 이전 투표 기록 등 반환하는 로직을 추가한다.

--- src/main/kotlin/com/goosesdream/golaping/common/websocket/dto/WebSocketInitialResponse.kt
+++ src/main/kotlin/com/goosesdream/golaping/common/websocket/dto/WebSocketInitialResponse.kt
@@ -3,5 +3,7 @@ package com.goosesdream.golaping.common.websocket.dto
 data class WebSocketInitialResponse(
     val voteLimit: Int,
     val voteEndTime: Long,
-    val webSocketSessionId: String? = null
+    val webSocketSessionId: String? = null,
+    val previousVotes: List<VoteOptionsData>,
+    val userVoteOptionIds: List<Long>
 )
