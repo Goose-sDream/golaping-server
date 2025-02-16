bf03019066c4a40b015d4251fc73182453663cc5
joonghyun <joongh@ewhain.net>
2025-02-16 23:30:18 +0900

    #126 feature: 로깅을 위해 LoggerUtil을 추가한다.

---
+++ src/main/kotlin/com/goosesdream/golaping/common/util/LoggerUtil.kt
@@ -0,0 +1,12 @@
+package com.goosesdream.golaping.common.util
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+inline fun <reified T> T.logger(): Logger {
+    return if (T::class.isCompanion) {
+        LoggerFactory.getLogger(T::class.java.enclosingClass)
+    } else {
+        LoggerFactory.getLogger(T::class.java)
+    }
+}
